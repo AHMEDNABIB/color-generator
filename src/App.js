8cec9294c0d0b541b7ea0f31b1fa65216533aa01
AHMEDNABIB <nabibpallab@gmail.com>
2021-11-02 01:30:57 +0600

    refactor: copy to clipboard

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import Values from 'values.js'
 function App() {
   const [color,setColor] = useState('');
   const [error, setError]= useState(false);
-  const[list,setList]=useState([]);
+  const[list,setList]=useState(new Values('#f15025').all(10));
 
   const handleSubmit=(e)=>{
     e.preventDefault();
    
     try{
-      let colors = new Values(colors).all(10)
-      console.log('Hello')
-    } catch (err){
+      let colors = new Values(color).all(10)
+      setList(colors)
+      console.log(colors)
+    } catch (error){
       setError(true);
-      console.log(err)
+      console.log(error)
     }
   }
 
@@ -25,13 +26,22 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} />
+          <input 
+             type="text" 
+             value={color} 
+             onChange={(e)=> setColor(e.target.value)} 
+             placeholder="#f15025" 
+             className={`${error ? 'error' : null}`} 
+             />
           <button className="btn" type="submit">submit</button>
         </form>
       </section>
 
-      <section>
-        <h4>List goes here</h4>
+      <section className="colors">
+       {list.map((color,index)=>{
+         console.log(color)
+         return <SingleColor key={index} {...color} index={index}/>
+       })}
       </section>
     </>
   )
